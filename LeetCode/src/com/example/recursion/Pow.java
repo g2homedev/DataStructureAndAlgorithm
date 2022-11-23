ac8eddf837f03c82f1c401bf9c9ee1e14f858afb
g2 <g2.home.dev@gmail.com>
2022-11-23 18:45:04 +0700

    LeetCode tutorial

---
+++ LeetCode/src/com/example/recursion/Pow.java
@@ -0,0 +1,24 @@
+package com.example.recursion;
+
+public class Pow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public double myPow(double x, int n) {
+		if (n == 0) {
+			return 1;
+		}
+
+		double temp = myPow(x, n / 2);
+
+		if (n % 2 == 0) {
+			return temp * temp;
+		}
+		return x * temp * temp;
+
+	}
+
+}
